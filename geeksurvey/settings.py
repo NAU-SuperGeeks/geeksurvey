5484c340f4a7e390eaae863a7f8eefd135d1f16a
timgiroux <timgiroux99@gmail.com>
2021-12-01 09:39:05 -0700

    add webserver to allowed hosts

--- geeksurvey/settings.py
+++ geeksurvey/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("GEEKSURVEY_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webapps-dev.ac.nau.edu']
 
 
 # Application definition
