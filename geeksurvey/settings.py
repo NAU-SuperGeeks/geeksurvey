ac72e07af1f27ec550a6292c67c2552e355c5a8f
timgiroux <timgiroux99@gmail.com>
2021-12-13 17:28:15 -0700

    frontend styles and login / signup

--- geeksurvey/settings.py
+++ geeksurvey/settings.py
@@ -16,6 +16,8 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- geeksurvey/views.py
+++ geeksurvey/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
 
 from .models import Example
 
 def index(request):
-    example_list = Example.objects.order_by('-pub_date')[:5]
-    context = {
-      'example_list': example_list,
-    }
-    return render(request, 'index.html', context)
+  example_list = Example.objects.order_by('-pub_date')[:5]
+  context = {
+    'example_list': example_list,
+  }
+  return render(request, 'home.html', context)
 
+def working(request):
+  context = {}
+  return render(request, 'working.html', context)
+
+def register(request):
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      username = form.cleaned_data.get('username')
+      login(request, user)
+    return redirect("home")
+
+  form = UserCreationForm
+  return render(request = request,
+                template_name = "register.html",
+                context={"form":form})
 
