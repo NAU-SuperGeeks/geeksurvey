b0f32a1f10ad077eb756c36eae85871fae9d07e4
Gustavo Valencia <71233830+GustavoMiguelValencia@users.noreply.github.com>
2022-01-21 10:39:40 -0700

    Add Level of Education field to User Profile

--- geeksurvey/forms.py
+++ geeksurvey/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio','age', 'user_exp']
+        fields = ['bio','age', 'user_exp', 'level_of_education']
