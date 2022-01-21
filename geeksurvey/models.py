c1f63e8b09cb9d21c73e5e343e4a36745aeeacee
Gustavo Valencia <71233830+GustavoMiguelValencia@users.noreply.github.com>
2022-01-21 12:13:34 -0700

    Add Occupation field to User Profile

--- geeksurvey/models.py
+++ geeksurvey/models.py
@@ -35,5 +35,23 @@ class Profile(models.Model):
         choices=LevelOfEducation.choices,
         default=LevelOfEducation.HIGH_SCHOOL)
 
+    # Defines choices in the Profile model class as recommended by Django Docs
+    class Occupation(models.TextChoices):
+        SOFTWARE_ENGINEER = 'SWE', _('Software Engineer/Developer')
+        DATA_SCIENTIST = 'DSCI', _('Data Scientist')
+        SYSTEMS_ADMINISTRATOR = 'SYSA', _('System Administrator/Analyst')
+        PRODUCT_MANAGER = 'PRDM', _('Product Manager')
+        NETWORK_ADMINISTRATOR = 'NWA', _('Network Administrator/Analyst')
+        DATABASE_ADMINISTRATOR = 'DBA', _('Database Administrator/Analyst')
+        SECURITY_ANALYST = 'SECA', _('Security Analyst')
+        PROFESSOR = 'PROF', _('Professor/Instructor')
+        RESEARCHER = 'RSCHR', _('Researcher')
+        SOFTWARE_TESTER = 'SWT', _('Software Tester')
+    # Adds occupation as a choosable field for the user profile
+    # TODO: May want to add an "other" option that is a write in. May require custom model.
+    occupation = models.CharField(max_length=5,
+        choices=Occupation.choices,
+        default=Occupation.SOFTWARE_ENGINEER)
+
     def __str__(self):
         return self.user.username
