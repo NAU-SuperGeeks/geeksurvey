fdcc8c37ca146583ab4ad5391fd17d550bfd4530
timgiroux <timgiroux99@gmail.com>
2022-02-12 11:21:09 -0700

    remove ability to change email and username

--- geeksurvey/views.py
+++ geeksurvey/views.py
@@ -52,12 +52,10 @@ def profile(request):
 def profile_update(request):
     profile = Profile.objects.get(user=request.user)
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
+        if p_form.is_valid():
             p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile') # Redirect back to profile page
@@ -68,7 +66,6 @@ def profile_update(request):
 
     context = {
         'profile': profile,
-        'u_form': u_form,
         'p_form': p_form
     }
 
